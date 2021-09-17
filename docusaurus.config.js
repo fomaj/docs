da3fa106b214fa6cd0f17855813a4368f69b0b27
Irshad Nilam <mail@irshadnilam.com>
2021-09-17 20:37:49 +0530

    Setup docusaurus

---
+++ docusaurus.config.js
@@ -0,0 +1,60 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(module.exports = {
+  title: 'Fomaj',
+  tagline: 'Predict and Win',
+  url: 'https:/dev.fomaj.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'fomaj', // Usually your GitHub org/user name.
+  projectName: 'fomaj', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js')
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Fomaj Docs',
+        logo: {
+          alt: 'Fomaj',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            href: 'https://dev.fomaj.com',
+            label: 'App',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [],
+        copyright: `Copyright © ${new Date().getFullYear()} Fomaj Finance.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+});
